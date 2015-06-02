121f842bdc3992df8cb7a4bdf17e854c2a3a92d1
vachanda <vachan.da@urbanladder.com>
2015-06-02 11:59:23 +0530

    Made changes to Line

--- lib/geo.rb
+++ lib/geo.rb
@@ -1,6 +1,6 @@
 class Line
 	
-	def set (x1,y1,x2,y2)
+	def initialize (x1,y1,x2,y2)
 		@x1 = x1
 		@y1 = y1
 		@x2 = x2
--- spec/geo_spec.rb
+++ spec/geo_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 describe 'Calulate length test 1' do	
-	 before :each do 
-	 	@line = Line.new
-	 end
 
 	 it "return 5 for values (2,3) and (5,7)" do
-	 		@line.set(2,3,5,7)
+	 		@line = Line.new(2,3,5,7)
 	 		expect(@line.length).to eq(5)
 	 end
+
 	 it "return 0 for values (1,1) and (1,1)" do
-	 		@line.set(1,1,1,1)
+	 		@line = Line.new(1,1,1,1)
 	 		expect(@line.length).to eq(0)
 	 end
 end
