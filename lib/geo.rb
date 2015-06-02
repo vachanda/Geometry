6dcd2515b165bbc66d3f5caef17d4937bf60e161
vachanda <vachan.da@urbanladder.com>
2015-06-02 11:51:57 +0530

    Calculate length of line

---
+++ lib/geo.rb
@@ -0,0 +1,16 @@
+class Line
+	
+	def set (x1,y1,x2,y2)
+		@x1 = x1
+		@y1 = y1
+		@x2 = x2
+		@y2 = y2
+	end
+
+	def length
+		x_comp = @x2 - @x1
+		y_comp = @y2 - @y1
+		s = x_comp * x_comp + y_comp * y_comp
+		return Math.sqrt(s)
+	end
+end
--- spec/geo_spec.rb
+++ spec/geo_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-describe 'display hello world' do
-	it { should run.and_return('hello world')}
+describe 'Calulate length test 1' do	
+	 before :each do 
+	 	@line = Line.new
+	 end
+
+	 it "return 5 for values (2,3) and (5,7)" do
+	 		@line.set(2,3,5,7)
+	 		expect(@line.length).to eq(5)
+	 end
+	 it "return 0 for values (1,1) and (1,1)" do
+	 		@line.set(1,1,1,1)
+	 		expect(@line.length).to eq(0)
+	 end
 end
