e344a6a35cf80f537d96dd74f5e712d22c6433e8
vachanda <vachan.da@urbanladder.com>
2015-06-02 15:04:02 +0530

    over-written the equality operator for Line class

--- lib/line.rb
+++ lib/line.rb
@@ -1,5 +1,6 @@
 class Line
 # JOB : Defines specification of a geometric line
+	attr_accessor :point1_x, :point1_y, :point2_x, :point2_y
 	def initialize(point1_x, point1_y, point2_x, point2_y)
 		@point1_x = point1_x
 		@point1_y = point1_y
@@ -15,6 +16,13 @@ class Line
 	end
 
 	def ==(line)
-		!! line
+		return !!line unless line
+		direct_compare_result = (@point2_x == line.point2_x && @point2_y == line.point2_y) &&
+		  (@point1_x == line.point1_x && @point1_y == line.point1_y)
+
+		cross_compare_result = (@point2_x == line.point1_x && @point2_y == line.point1_y) &&
+		  (@point1_x == line.point2_x && @point1_y == line.point2_y)
+
+		direct_compare_result || cross_compare_result
 	end
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -30,6 +30,12 @@ describe "Test for same lines" do
 	it "The line wtih end point (1,1), (3,4) and (1,1), (3,4) are same" do
 		line1 = Line.new(1,1,3,4)
 		line2 = Line.new(1,1,3,4)
-		expect(line1 == line2).to eq(true)
+		expect(line1).to eq(line2)
+	end
+
+	it "The line wtih end point (1,1), (3,4) and (2,2), (4,5) are not same" do
+		line1 = Line.new(1,1,3,4)
+		line2 = Line.new(2,2,4,5)
+		expect(line1).to_not eq(line2)
 	end
 end
