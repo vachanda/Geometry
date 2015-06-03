43a9cc95d676d87681fa717d88b251b4e964910f
vachanda <vachan.da@urbanladder.com>
2015-06-03 11:19:01 +0530

    added changes to square

--- spec/geometry/square_spec.rb
+++ spec/geometry/square_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe "Square functionality check" do	
 
-	it "Square check: length = 1" do
+	it "Perimeter of the Square with length 1 and end point (1, 1) is 4" do
 		point = Geometry::Point.new(1, 1)
-		length = 1
+		length = Random.rand(10) + 1
 		square = Geometry::Square.new(point, length)
-		expect(square.perimeter).to eq(4)
+		expect(square.perimeter).to eq(4*length)
 	end
-	it "Square check: length = 1" do
+
+	it "Area of the Square with length 1 and end point (1, 1) is 1" do
 		point = Geometry::Point.new(1, 1)
-		length = 1
+		length = Random.rand(10) + 1
 		square = Geometry::Square.new(point, length)
-		expect(square.area).to eq(1)
+		expect(square.area).to eq(length ** 2)
 	end
+
 end
